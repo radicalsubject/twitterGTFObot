444061afc52601e15640b68062623a11bfc93e42
radicalsubject <olegteravolt@gmail.com>
2021-02-19 12:24:45 +0300

    Update token_extractor.py

--- bot_container/token_extractor.py
+++ bot_container/token_extractor.py
@@ -3,5 +3,5 @@ try:
     f = open("token.secret", "r")
     token = f.read()
 except:
-    # token = os.environ["BOT_TOKEN"]
-    token = "1519348433:AAHa_065sbf3b32krSINHs5saDC_4M2kD8Q"
+    token = os.environ["BOT_TOKEN"]
+    # token = "1519348433:AAHa_065sbf3b32krSINHs5saDC_4M2kD8Q"
